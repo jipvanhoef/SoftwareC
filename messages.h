be6266b4abd9c7b350a30a10cf0c1a89ceed6248
ThymoFieten <thymofieten@gmail.com>
2022-11-22 14:10:07 +0100

    added sample code

---
+++ messages.h
@@ -0,0 +1,15 @@
+/* 
+ * Operating Systems {2INCO} Practical Assignment
+ * Interprocess Communication
+ *
+ * Contains definitions of message data structures
+ *
+ */
+
+#ifndef MESSAGES_H
+#define MESSAGES_H
+
+// define the data structures for your messages here
+
+#endif
+
