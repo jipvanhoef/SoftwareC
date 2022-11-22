1a5953058fdcd7dd053695f5f848d11ce99893e8
FriendedBreak <Thymofieten@gmail.com>
2022-11-22 15:35:05 +0100

    added some code

--- messages.h
+++ messages.h
@@ -10,6 +10,20 @@
 #define MESSAGES_H
 
 // define the data structures for your messages here
+typedef struct
+{
+    // a data structure with 3 members
+    int                     a;
+    int                     b;
+    char                    c;
+} MQ_REQUEST_MESSAGE;
 
+typedef struct
+{
+    // a data structure with 3 members
+    int                     e;
+    char                    f[20];
+    char                    g[20];
+} MQ_RESPONSE_MESSAGE;
 #endif
 
