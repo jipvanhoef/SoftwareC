ef31197fece857b5fa8880eb86edba102d2ebcde
Koert <schoenmakers.koert@gmail.com>
2022-12-07 21:25:31 +0100

    Coding style and uncommented the perrors

--- worker_s2.c
+++ worker_s2.c
@@ -37,51 +37,48 @@ int main (int argc, char * argv[])
     
     // Open the request message queue
     mq_fd_request = mq_open(argv[0], O_RDONLY);
-    //perror("mq_fd_request failed to open in worker_1")
+    perror("mq_fd_request failed to open in worker_2")
 
     // Check if the request message queue is opened
-    if (mq_fd_request == -1)
-    {
-        // perror ("mq_open() for request failed in worker_s1.c");
+    if (mq_fd_request == -1) {
+        perror ("mq_open() for request failed in worker_s2.c");
         exit (0);
     }
 
     // Open the response message queue
-    mq_fd_response = mq_open(argv[1], O_WRONLY); 
+    mq_fd_response = mq_open(argv[1], O_WRONLY);
+    perror("mq_fd_response failed to open in worker_2")
 
     // Check if the response message queue is opened
-    if (mq_fd_response == -1)
-    {
-        // perror ("mq_open() for response queue failed in worker_s1.c");
+    if (mq_fd_response == -1) {
+        perror ("mq_open() for response queue failed in worker_s2.c");
         exit (0);
     }
 
     //while there are messages in the queue retrieve them
     while (true)
     {
-        if (mq_receive (mq_fd_request, (char *)&req, sizeof(req), NULL) > -1){
-            if(req.data == -1 && req.RequestID == -1 && req.ServiceID == -1){
+        if (mq_receive (mq_fd_request, (char *)&req, sizeof(req), NULL) > -1) {
+            if (req.data == -1 && req.RequestID == -1 && req.ServiceID == -1) {
                 //close the message queue
                 mq_close(mq_fd_response);
                 mq_close(mq_fd_request);
                 return(0);
             }
-            //perror("mq_receive worker 2");
+            perror("mq_receive worker 2");
             //sleep for 10000 ms
             rsleep(10000);
             //calculate the result of the service
             int result = service(req.data);
-            //printf("w1 result: %u\n", result);
             //create the response message
             rsp.RequestID = req.RequestID;
             rsp.result = result;
             //send the response message
             mq_send(mq_fd_response, (char *)&rsp, sizeof(rsp), NULL);
-            //perror("sending w1");
-
-            //perror("Worker trying to send in worker_2");    
+            perror("sending w2");
         }
-    };
+    }
+
     return (0);
 }
 
